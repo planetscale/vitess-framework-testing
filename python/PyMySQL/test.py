ddb8c1e7c792cd26cb570c417d291998869b930a
Mike Cronce <mc@planetscale.com>
2020-10-15 11:59:56 -0400

    python/PyMySQL/test.py:  rm try/catch around everything, we want the process to exit nonzero if something fails

--- python/PyMySQL/test.py
+++ python/PyMySQL/test.py
@@ -19,56 +19,53 @@ connection = pymysql.connect(
 	db = parsed.path.strip('/')
 )
 
-try:
-	with connection.cursor() as cursor:
-		query = 'CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)'
-		cursor.execute(query)
-	connection.commit()
+with connection.cursor() as cursor:
+	query = 'CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL)'
+	cursor.execute(query)
+connection.commit()
 
-	with connection.cursor() as cursor:
-		query = 'INSERT INTO people VALUES (DEFAULT, %s)'
-		cursor.execute(query, ('Vitess User 1',))
-		cursor.execute(query, ('Vitess User 2',))
-		cursor.execute(query, ('Vitess User 3',))
-	connection.commit()
+with connection.cursor() as cursor:
+	query = 'INSERT INTO people VALUES (DEFAULT, %s)'
+	cursor.execute(query, ('Vitess User 1',))
+	cursor.execute(query, ('Vitess User 2',))
+	cursor.execute(query, ('Vitess User 3',))
+connection.commit()
 
-	with connection.cursor() as cursor:
-		query = 'SELECT * FROM people'
-		cursor.execute(query)
-		assert(cursor.fetchone() == (1, 'Vitess User 1'))
-		assert(cursor.fetchone() == (2, 'Vitess User 2'))
-		assert(cursor.fetchone() == (3, 'Vitess User 3'))
-		assert(cursor.fetchone() is None)
-	
-	with connection.cursor() as cursor:
-		query = 'UPDATE people SET name = %s WHERE id = %s'
-		cursor.execute(query, ('NotVitess User', 2))
-	connection.commit()
-	
-	with connection.cursor() as cursor:
-		query = 'SELECT * FROM people'
-		cursor.execute(query)
-		assert(cursor.fetchone() == (1, 'Vitess User 1'))
-		assert(cursor.fetchone() == (2, 'NotVitess User'))
-		assert(cursor.fetchone() == (3, 'Vitess User 3'))
-		assert(cursor.fetchone() is None)
-	
-	with connection.cursor() as cursor:
-		query = 'DELETE FROM people WHERE id = %s'
-		cursor.execute(query, (2,))
-	connection.commit()
-	
-	with connection.cursor() as cursor:
-		query = 'SELECT * FROM people'
-		cursor.execute(query)
-		assert(cursor.fetchone() == (1, 'Vitess User 1'))
-		assert(cursor.fetchone() == (3, 'Vitess User 3'))
-		assert(cursor.fetchone() is None)
-	
-	with connection.cursor() as cursor:
-		query = 'DROP TABLE people'
-		cursor.execute(query)
-	connection.commit()
-finally:
-	connection.close()
+with connection.cursor() as cursor:
+	query = 'SELECT * FROM people'
+	cursor.execute(query)
+	assert(cursor.fetchone() == (1, 'Vitess User 1'))
+	assert(cursor.fetchone() == (2, 'Vitess User 2'))
+	assert(cursor.fetchone() == (3, 'Vitess User 3'))
+	assert(cursor.fetchone() is None)
+
+with connection.cursor() as cursor:
+	query = 'UPDATE people SET name = %s WHERE id = %s'
+	cursor.execute(query, ('NotVitess User', 2))
+connection.commit()
+
+with connection.cursor() as cursor:
+	query = 'SELECT * FROM people'
+	cursor.execute(query)
+	assert(cursor.fetchone() == (1, 'Vitess User 1'))
+	assert(cursor.fetchone() == (2, 'NotVitess User'))
+	assert(cursor.fetchone() == (3, 'Vitess User 3'))
+	assert(cursor.fetchone() is None)
+
+with connection.cursor() as cursor:
+	query = 'DELETE FROM people WHERE id = %s'
+	cursor.execute(query, (2,))
+connection.commit()
+
+with connection.cursor() as cursor:
+	query = 'SELECT * FROM people'
+	cursor.execute(query)
+	assert(cursor.fetchone() == (1, 'Vitess User 1'))
+	assert(cursor.fetchone() == (3, 'Vitess User 3'))
+	assert(cursor.fetchone() is None)
+
+with connection.cursor() as cursor:
+	query = 'DROP TABLE people'
+	cursor.execute(query)
+connection.commit()
 
