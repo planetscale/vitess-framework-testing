a7484129e1ac4be7519ab87d0361078278523682
Mike Cronce <mc@planetscale.com>
2020-10-21 15:41:58 -0400

    javascript/sequelize/src:  Use DATABASE_URL environment variable

--- javascript/sequelize/src/models/index.js
+++ javascript/sequelize/src/models/index.js
@@ -1,8 +1,7 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json');
 const db = {};
 
 let sequelize;
