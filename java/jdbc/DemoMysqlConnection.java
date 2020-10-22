47ea89555f125cc042c1b68fb3792dcf57acb47a
Sam Kottler <skottler@github.com>
2020-10-22 15:14:27 -0400

    Bootstrap JDBC support (#10)
    * Add basic gitignore for jdbc bits
    
    * Add DemoMysqlConnection that loads the env
    
    * Add test wrapper that doesn't do anything yet
    
    * Get rid of getString stuff
    
    * Add functional dockerfile
    
    * Add entrypoint to compile and run
    
    * Make the mount r/w and pass through env vars
    
    * Use /src consistently
    
    * Put the one file into the main directory
    
    * Exit 1 when there's an exception
    
    * Consistent curlies
    
    * Break versioning out into env vars
    
    * Create people table with schema
    
    * Add inserts into people table
    
    * Outline of the steps and SQL that will be run
    
    * Add initial assert around record count
    
    * Fix the variable name
    
    * A few minor fixes
    
    * Fix selectedCount behavior
    
    Co-authored-by: Sam Kottler <skottler@planetscale.com>

---
+++ java/jdbc/DemoMysqlConnection.java
@@ -0,0 +1,63 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class DemoMysqlConnection {
+	public static void main(String args[]) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String vtUsername = System.getenv("VT_USERNAME");
+			String vtPassword = System.getenv("VT_PASSWORD");
+			String vtHost = System.getenv("VT_HOST");
+			String vtPort = System.getenv("VT_PORT");
+			String vtDatabase = System.getenv("VT_DATABASE");
+
+			String connectionUri = "jdbc:mysql://" + vtHost + ":" + vtPort + "/" + vtDatabase + "?serverTimezone=UTC";
+			Connection con = DriverManager.getConnection(connectionUri, vtUsername, vtPassword);
+
+			String createTableSql = "CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL) ";
+			String insertUser1Sql = "INSERT INTO people VALUES (1, 'Vitess User 1')";
+			String insertUser2Sql = "INSERT INTO people VALUES (2, 'Vitess User 2')";
+			String insertUser3Sql = "INSERT INTO people VALUES (3, 'Vitess User 3')";
+			String selectPeopleSql = "SELECT COUNT(*) FROM people";
+			String updatePeopleSql = "UPDATE people SET name='Vitess User 500' where id=2";
+			String dropPeopleSql = "DROP TABLE people";
+
+			Statement st = con.createStatement();
+			st.getConnection();
+
+			// Create table
+			st.executeUpdate(createTableSql);
+
+			// Insert three records into people table
+			st.executeUpdate(insertUser1Sql);
+			st.executeUpdate(insertUser2Sql);
+			st.executeUpdate(insertUser3Sql);
+
+			// Select * from people
+			ResultSet rs = st.executeQuery(selectPeopleSql);
+			rs.next();
+			int selectedCount = rs.getInt(1);
+			assert selectedCount == 3;
+
+			// Update records
+			st.executeUpdate(updatePeopleSql);
+
+			// Select * again
+			st.executeQuery(selectPeopleSql);
+
+			// Drop the table
+			st.getConnection();
+			st.executeUpdate(dropPeopleSql);
+
+			st.close();
+			con.close();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
