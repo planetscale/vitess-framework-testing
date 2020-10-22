aaef4e6ad220c4a964f6aa600e073d48b7e5e19b
Sam Kottler <skottler@planetscale.com>
2020-10-22 10:06:04 -0400

    Add DemoMysqlConnection that loads the env

---
+++ java/jdbc/src/DemoMysqlConnection.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class DemoMysqlConnection {
+	public static void main(String args[]){
+		try{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			String vtUsername = System.getenv("VT_USERNAME");
+			String vtPassword = System.getenv("VT_PASSWORD");
+			String vtHost = System.getenv("VT_HOST");
+			String vtPort = System.getenv("VT_PORT");
+			String vtDatabase = System.getenv("VT_DATABASE");
+
+			String connectionUri = "jdbc:mysql://" + vtHost + ":" + vtPort + "/" + vtDatabase;
+			Connection con = DriverManager.getConnection(connectionUri, vtUsername, vtPassword);
+			
+			Statement st = con.createStatement();
+			ResultSet rs = st.executeQuery("select * from login");
+			
+			while(rs.next()){
+				System.out.println(rs.getString(1)+" "+rs.getString(2)+" "+rs.getString(3));
+			}
+			
+			con.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+}
