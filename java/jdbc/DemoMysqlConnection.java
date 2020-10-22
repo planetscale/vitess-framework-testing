c594a5f557a0c4ad4a3114fcd4c32223580d714e
Sam Kottler <skottler@planetscale.com>
2020-10-22 15:11:01 -0400

    A few minor fixes

--- java/jdbc/DemoMysqlConnection.java
+++ java/jdbc/DemoMysqlConnection.java
@@ -14,14 +14,14 @@ class DemoMysqlConnection {
 			String vtPort = System.getenv("VT_PORT");
 			String vtDatabase = System.getenv("VT_DATABASE");
 
-			String connectionUri = "jdbc:mysql://" + vtHost + ":" + vtPort + "/" + vtDatabase;
+			String connectionUri = "jdbc:mysql://" + vtHost + ":" + vtPort + "/" + vtDatabase + "?serverTimezone=UTC";
 			Connection con = DriverManager.getConnection(connectionUri, vtUsername, vtPassword);
 
 			String createTableSql = "CREATE TABLE people (id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255) NOT NULL) ";
 			String insertUser1Sql = "INSERT INTO people VALUES (1, 'Vitess User 1')";
 			String insertUser2Sql = "INSERT INTO people VALUES (2, 'Vitess User 2')";
 			String insertUser3Sql = "INSERT INTO people VALUES (3, 'Vitess User 3')";
-			String selectPeopleSql = "SELECT COUNT(*) as rowcount FROM people";
+			String selectPeopleSql = "SELECT COUNT(*) FROM people";
 			String updatePeopleSql = "UPDATE people SET name='Vitess User 500' where id=2";
 			String dropPeopleSql = "DROP TABLE people";
 
@@ -35,17 +35,14 @@ class DemoMysqlConnection {
 			st.executeUpdate(insertUser1Sql);
 			st.executeUpdate(insertUser2Sql);
 			st.executeUpdate(insertUser3Sql);
-			con.commit();
 
 			// Select * from people
-			ResultSet people = st.executeQuery(selectPeopleSql);
-			people.getRow();
-			Integer selectedCount = people.getInt("rowcount");
+			ResultSet rs = st.executeQuery(selectPeopleSql);
+			int selectedCount = rs.last() ? rs.getRow() : 0;
 			assert selectedCount == 3;
 
 			// Update records
 			st.executeUpdate(updatePeopleSql);
-			con.commit();
 
 			// Select * again
 			st.executeQuery(selectPeopleSql);
@@ -53,7 +50,6 @@ class DemoMysqlConnection {
 			// Drop the table
 			st.getConnection();
 			st.executeUpdate(dropPeopleSql);
-			con.commit();
 
 			st.close();
 			con.close();
