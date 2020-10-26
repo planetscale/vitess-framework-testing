c682b3188276d7164f7828482cacc3ad670a15e2
Mike Cronce <mc@planetscale.com>
2020-10-26 15:23:13 -0400

    javascript/sequelize:  This was super broken; fixed it

--- javascript/sequelize/src/models/index.js
+++ javascript/sequelize/src/models/index.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const config = require(__dirname + '/../config/config.json');
+let config = require(__dirname + '/../config/config.js');
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  const env = process.env.NODE_ENV || 'development';
+  config = config[env];
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
