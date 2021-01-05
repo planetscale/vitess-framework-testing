0c0b742b669751435e2d8b6449ac39e610745dfa
Mike Cronce <mc@planetscale.com>
2021-01-05 14:08:42 -0500

    frameworks/ruby/rails6/run.sh:  Move user:create and user:get[101] to the end

--- frameworks/ruby/rails6/run.sh
+++ frameworks/ruby/rails6/run.sh
@@ -2,10 +2,10 @@
 
 rake db:migrate;
 rake db:seed;
-rake user:create;
-rake 'user:get[101]';
 rake user:list;
 rake 'user:get[1]';
 rake user:delete_first;
 rake user:list | head;
+rake user:create;
+rake 'user:get[101]';
 
