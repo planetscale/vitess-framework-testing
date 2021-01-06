6d5388632c43385c3c14b176d599693df05abf8b
Mike Cronce <mc@planetscale.com>
2021-01-06 12:27:39 -0500

    frameworks/ruby/rails6/run.sh:  + TODO about data validation

--- frameworks/ruby/rails6/run.sh
+++ frameworks/ruby/rails6/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -ex
 set -o pipefail;
 
+# TODO:  Validate that each step produces data that we expect, instead of just
+#    displaying it to the user and failing if the server rejects a query
+
 rake db:migrate;
 rake db:seed;
 rake user:list;
