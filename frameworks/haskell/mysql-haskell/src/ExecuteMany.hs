c1eb8dae53952b3891d034644ad6f4efcf382984
Mike Cronce <mc@planetscale.com>
2021-01-20 17:56:31 -0500

    frameworks/haskell/mysql-haskell/src/ExecuteMany.hs:  Don't exceed Vitess's default row limit (#100)

--- frameworks/haskell/mysql-haskell/src/ExecuteMany.hs
+++ frameworks/haskell/mysql-haskell/src/ExecuteMany.hs
@@ -45,7 +45,7 @@ tests c = do
             \?     ,\
             \?     ,\
             \?)"
-            (replicate 50000
+            (replicate 9999
                 [ MySQLInt32 0
                 , MySQLBit 255
                 , MySQLInt8 (-128)
@@ -78,4 +78,4 @@ tests c = do
                 , MySQLText "foo,bar"
                 ]
             )
-    assertEqual "executeMany affected rows" (sum $ map okAffectedRows oks) 50000
+    assertEqual "executeMany affected rows" (sum $ map okAffectedRows oks) 9999
