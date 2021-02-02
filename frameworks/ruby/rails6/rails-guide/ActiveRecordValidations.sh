bd216def1eb2e74de94387aa0d24949174ff2d4e
Mike Cronce <mc@planetscale.com>
2021-02-01 19:59:50 -0500

    frameworks/ruby/rails6/rails-guide/ActiveRecordValidations.sh:  Run all the chapter 2 stuff

--- frameworks/ruby/rails6/rails-guide/ActiveRecordValidations.sh
+++ frameworks/ruby/rails6/rails-guide/ActiveRecordValidations.sh
@@ -11,3 +11,23 @@ rake guide_validation:step_1_3 # Skipping Validations
 rake guide_validation:step_1_4 # valid? and invalid?
 rake guide_validation:step_1_5 # errors[]
 
+# 2 Validation Helpers
+rails generate migration CreatePerson2 terms_of_service:string eula:string
+rails generate migration CreatePerson3 email:string
+rails generate migration CreatePerson4 opt_in:string
+rails generate migration CreateNumbers integer:integer float:float string:string
+rails db:migrate
+rake guide_validation:step_2_1 # acceptance
+rake guide_validation:step_2_2 # validates_associated
+rake guide_validation:step_2_3 # confirmation
+rake guide_validation:step_2_4 # exclusion
+rake guide_validation:step_2_5 # format
+rake guide_validation:step_2_6 # inclusion
+rake guide_validation:step_2_7 # length
+rake guide_validation:step_2_8 # numericality
+rake guide_validation:step_2_9 # presence
+rake guide_validation:step_2_10 # absence
+rake guide_validation:step_2_11 # uniqueness
+rake guide_validation:step_2_12 # validates_with
+rake guide_validation:step_2_13 # validates_each
+
