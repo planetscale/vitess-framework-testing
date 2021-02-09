a00d3fd4c08cf4f49683b4538710ab6ad82762fb
Mike Cronce <mc@planetscale.com>
2021-02-09 15:48:17 -0500

    frameworks/ruby/rails6/src/db/migrate/20210208114300_add_author_and_book2s.rb:  Switch from standalone add_reference and add_foreign_key calls to belongs_to

--- frameworks/ruby/rails6/src/db/migrate/20210208114300_add_author_and_book2s.rb
+++ frameworks/ruby/rails6/src/db/migrate/20210208114300_add_author_and_book2s.rb
@@ -1,17 +1,15 @@
 class AddAuthorAndBook2s < ActiveRecord::Migration[6.1]
   def change
-    create_table :author do |t|
+    create_table :authors do |t|
       t.string :name
       t.timestamps
     end
 
     create_table :book2s do |t|
       t.string :title
+      t.belongs_to :author, foreign_key: true
       t.timestamps
     end
-
-    add_reference :book2s, :author
-    add_foreign_key :book2s, :author
   end
 end
 
