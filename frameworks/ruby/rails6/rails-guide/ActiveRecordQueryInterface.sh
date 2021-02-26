5b15cc7ba2a35451dd20a96f776475d47fa915d7
Mike Cronce <mc@planetscale.com>
2021-02-26 15:28:02 -0500

    frameworks/ruby/rails6/rails-guide/ActiveRecordQueryInterface.sh:  + chapter 3

--- frameworks/ruby/rails6/rails-guide/ActiveRecordQueryInterface.sh
+++ frameworks/ruby/rails6/rails-guide/ActiveRecordQueryInterface.sh
@@ -9,3 +9,10 @@ rake guide_query_interface:seed
 rake guide_query_interface:step_2_1 # Retrieving a Single Object
 rake guide_query_interface:step_2_2 # Retrieving Multiple Objects in Batches
 
+# 3 Conditions
+rake guide_query_interface:step_3_1 # Pure String Conditions
+rake guide_query_interface:step_3_2 # Array Conditions
+rake guide_query_interface:step_3_3 # Hash Conditions
+rake guide_query_interface:step_3_4 # NOT Conditions
+rake guide_query_interface:step_3_5 # OR Conditions
+
