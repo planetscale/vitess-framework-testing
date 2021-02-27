2de70efddaed2eeff315ec36cffa37cbce5ed187
Mike Cronce <mc@planetscale.com>
2021-02-27 01:10:21 -0500

    frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb:  Fix status order

--- frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb
+++ frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb
@@ -2,7 +2,7 @@ class Order2 < ApplicationRecord
 	belongs_to :customer, class_name: :Customer2, foreign_key: :customer2_id, counter_cache: :orders_count, inverse_of: :orders
 	has_and_belongs_to_many :books, class_name: :Book6, join_table: :book6s_order2s
 
-	enum status: [:shipped, :being_packed, :complete, :cancelled]
+	enum status: [:being_packed, :shipped, :complete, :cancelled]
 
 	scope :created_before, ->(time) { where('created_at < ?', time) }
 end
