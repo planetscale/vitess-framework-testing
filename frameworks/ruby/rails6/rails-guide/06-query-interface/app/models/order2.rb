67bd1c89c2b9782408fc99f55ef5bbeae3f60d9e
Mike Cronce <mc@planetscale.com>
2021-03-01 18:10:27 -0500

    frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb:  Add conditional to created_before scope

--- frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb
+++ frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/order2.rb
@@ -4,7 +4,7 @@ class Order2 < ApplicationRecord
 
 	enum status: [:being_packed, :shipped, :complete, :cancelled]
 
-	scope :created_before, ->(time) { where('created_at < ?', time) }
+	scope :created_before, ->(time) { where('created_at < ?', time) if time.present? }
 	scope :created_in_time_range, ->(time_range) { where(created_at: time_range) }
 end
 
