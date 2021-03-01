b6ce185b83573570b2d7bfeb78a71be76fa92017
Mike Cronce <mc@planetscale.com>
2021-03-01 18:11:06 -0500

    frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/book6.rb:  + newish scope, and changed out_of_print_and_expensive to use costs_more_than rather than its own where(price)

--- frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/book6.rb
+++ frameworks/ruby/rails6/rails-guide/06-query-interface/app/models/book6.rb
@@ -7,7 +7,8 @@ class Book6 < ApplicationRecord
 	scope :in_print, -> { where(out_of_print: false) }
 	scope :out_of_print, -> { where(out_of_print: true) }
 	scope :old, -> { where('year_published < ?', 50.years.ago) }
-	scope :out_of_print_and_expensive, -> { out_of_print.where('price > 500') }
+	scope :newish, -> { where('year_published >= ?', 50.years.ago) }
 	scope :costs_more_than, ->(amount) { where('price > ?', amount) }
+	scope :out_of_print_and_expensive, -> { out_of_print.costs_more_than(500) }
 end
 
