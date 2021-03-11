16ce56ec2bcb1a0a75279f7642751da54e48b073
Mike Cronce <mc@planetscale.com>
2021-03-10 20:32:19 -0500

    frameworks/rust/mysql_async/src/src/main.rs:  rm COLLATE clause from final CREATE TABLE

--- frameworks/rust/mysql_async/src/src/main.rs
+++ frameworks/rust/mysql_async/src/src/main.rs
@@ -225,7 +225,7 @@ async fn main() {
 		`one` int NOT NULL,
 		`two` int NOT NULL,
 		PRIMARY KEY (`one`,`two`)
-	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4
 	";
 	println!("--- query:{}", query);
 	conn.query_drop(query).await.expect("CREATE TABLE `s` failed");
