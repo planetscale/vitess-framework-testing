70e7f0dfc48f8e3a0812a786a40253cc1988c596
GuptaManan100 <manan@planetscale.com>
2021-04-19 12:47:30 +0530

    finished active records associations guide
    Signed-off-by: GuptaManan100 <manan@planetscale.com>

--- frameworks/ruby/rails6/rails-guide/initialVschemaSetup.sh
+++ frameworks/ruby/rails6/rails-guide/initialVschemaSetup.sh
@@ -204,3 +204,36 @@ mysql_run "alter vschema on test.book5s add vindex book5s_keyspace_id(id) using 
 # Use the vindex created above for sharding chapter5s, more info at https://vitess.io/docs/user-guides/vschema-guide/lookup-as-primary/
 mysql_run "alter vschema on test.chapters add vindex book5s_keyspace_id(book5_id)"
 add_sequence_table "chapters"
+
+# explicit commands here because the primary key is called guid instead of id
+mysql_run "alter vschema on test.supplier3s add vindex \`binary_md5\`(guid) using \`binary_md5\`;"
+mysql_run "create table unsharded.supplier3s_seq(id bigint, next_id bigint, cache bigint, primary key(id)) comment 'vitess_sequence'"
+mysql_run "insert into unsharded.supplier3s_seq(id, next_id, cache) values(0, 1, 3)"
+mysql_run "alter vschema add sequence unsharded.supplier3s_seq"
+mysql_run "alter vschema on test.supplier3s add auto_increment guid using unsharded.supplier3s_seq"
+# id used here for vindex and not supplier_id, because the tests change this field and updating primary vindex is not supported
+add_binary_md5_vindex "account4s" "id"
+add_sequence_table "account4s"
+
+# similar structure seen previously
+add_sequence_and_vindex "supplier4s"
+add_binary_md5_vindex "account5s" "supplier4_id"
+add_sequence_table "account5s"
+# Add consistent_lookup_unique vindex, more info at https://vitess.io/docs/user-guides/vschema-guide/unique-lookup/
+mysql_run "create table unsharded.account5s_keyspace_id(id bigint not null auto_increment, keyspace_id varbinary(20), primary key(id))"
+mysql_run "alter vschema add table unsharded.account5s_keyspace_id"
+mysql_run "alter vschema on test.account5s add vindex account5s_keyspace_id(id) using consistent_lookup_unique with owner=\`account5s\`, table=\`unsharded.account5s_keyspace_id\`, from=\`id\`, to=\`keyspace_id\`"
+# Use the vindex created above for sharding account_histories, more info at https://vitess.io/docs/user-guides/vschema-guide/lookup-as-primary/
+mysql_run "alter vschema on test.transactions add vindex account5s_keyspace_id(account5_id)"
+add_sequence_table "transactions"
+
+# Since there are 2 foreign keys from puppers_friends to puppers, we cannot shard by just one of them
+# The only way the foreign keys can be enforced if the entire tables live in a single shard
+mysql_run "alter vschema on test.puppers add vindex \`null\`(id) using \`null\`"
+mysql_run "alter vschema on test.puppers_friends add vindex \`null\`(this_pupper_id) using \`null\`"
+
+add_sequence_and_vindex "assembly5s"
+add_sequence_and_vindex "part5s"
+add_sequence_and_vindex "manufacturers"
+add_binary_md5_vindex "assembly5s_part5s" "part5_id"
+add_binary_md5_vindex "manufacturers_part5s" "part5_id"
