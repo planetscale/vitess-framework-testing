1a567e5a328f5151d8e6590430613eda46429e13
Abhay Katheria <40055274+abhaykatheria@users.noreply.github.com>
2021-06-22 14:13:25 +0530

    Test Django model inheritances (#157)
    * Test Django model inheritances
    
    this commit adds tests for model inheritance works in
    django.
    
    Signed-off-by: abhaykatheria <abhay.katheria1998@gmail.com>
    
    * Added tests for proxy models and multiple inheritance
    
    Signed-off-by: abhaykatheria <abhay.katheria1998@gmail.com>
    
    * minor change in frameworks/python/django/src/testApp/test/test_model_inheritence/test_inheritence.py
    
    Co-authored-by: Mike Cronce <floppie@quadra-tec.net>
    
    Co-authored-by: Mike Cronce <floppie@quadra-tec.net>

--- frameworks/python/django/src/testApp/models.py
+++ frameworks/python/django/src/testApp/models.py
@@ -50,6 +50,97 @@ class Server(models.Model):
     def __str__(self):
         return self.name
 
+#  https://docs.djangoproject.com/en/3.2/topics/db/models/#model-inheritance
+
+class CommonInfo(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.PositiveIntegerField()
+
+    class Meta:
+        abstract = True
+        ordering = ["age"]
+
+    def __str__(self):
+        return "%s %s" % (self.__class__.__name__, self.name)
+
+
+class Worker(CommonInfo):
+    job = models.CharField(max_length=50)
+
+
+class Unmanaged(models.Model):
+    guardian = models.CharField(max_length=100)
+
+    class Meta:
+        abstract = True
+        ordering = ["guardian"]
+        managed = False
+
+
+class Student(CommonInfo):
+    school_class = models.CharField(max_length=10)
+
+    class Meta(CommonInfo.Meta):
+        pass
+
+
+# Abstract base classes with related models
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Attachment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        models.CASCADE,
+        
+        related_name="%(app_label)s_%(class)s_related",
+        related_query_name="%(app_label)s_%(class)ss"
+    )
+    content = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Comment(Attachment):
+    is_spam = models.BooleanField(default=False)
+
+
+class Link(Attachment):
+    url = models.URLField()
+
+
+# multi-table inheritance
+
+
+class Chef(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Place(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=80)
+
+
+class Rating(models.Model):
+    rating = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["-rating"]
+
+
+class Restaurant(Place, Rating):
+    serves_hot_dogs = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+    chef = models.ForeignKey(Chef, models.SET_NULL, null=True, blank=True)
+
+    class Meta(Rating.Meta):
+        db_table = "my_restaurant"
+
 class HQAddress(models.Model):
     house_no = models.CharField(max_length=100)
     street = models.CharField(max_length=100)
@@ -93,3 +184,29 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(to=Author)
+
+# proxy models
+
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+
+    def do_something(self):
+        # ...
+        pass
+
+class OrderedPerson(Person):
+    class Meta:
+        ordering = ["last_name"]
+        proxy = True
+
+# multiple inheritance
+class Product:
+    product_id = models.AutoField(primary_key=True)
+
+class Review(models.Model):
+    review_id = models.AutoField(primary_key=True)
+    ...
+
+class ProductReview(Product, Review):
+    pass
---
+++ frameworks/python/django/src/testApp/test/test_model_inheritence/test_inheritence.py
@@ -0,0 +1,132 @@
+#  Django's guide on Model layer
+#  https://docs.djangoproject.com/en/3.2/topics/db/models/#model-inheritance
+
+from django.test import TestCase
+from testApp.models import (
+    CommonInfo,
+    Worker,
+    Student,
+    Post,
+    Restaurant,
+    Place,
+    Chef,
+    Person,
+    MyPerson,
+    OrderedPerson,
+    ProductReview
+)
+from django.core.exceptions import FieldError
+
+
+class TestModelInheritance(TestCase):
+    def setUp(self):
+        self.test_worker_1 = Worker.objects.create(name="John", age=45, job="tester")
+        self.test_worker_2 = Worker.objects.create(name="Jane", age=40, job="QnA")
+
+        self.test_student_1 = Student.objects.create(
+            name="Bobby", age=15, school_class="9A"
+        )
+        self.test_student_2 = Student.objects.create(
+            name="Sunny", age=17, school_class="12B"
+        )
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.restaurant = Restaurant.objects.create(
+            name="Central Perk",
+            address="123 NYC",
+            serves_hot_dogs=True,
+            serves_pizza=False,
+            rating=2,
+        )
+
+        chef = Chef.objects.create(name="Ted")
+
+        cls.another_restaurant = Restaurant.objects.create(
+            name="McLarens",
+            address="1234 NYC",
+            serves_hot_dogs=False,
+            serves_pizza=False,
+            rating=4,
+            chef=chef,
+        )
+
+    def test_basic_inheritance(self):
+        # this tests that both worker and student class inherited
+        # the __str__() of the base class.
+        self.assertEqual(str(self.test_worker_1), "Worker John")
+        self.assertEqual(str(self.test_student_1), "Student Bobby")
+
+    def test_meta_inheritance_ordering(self):
+        # this tests that both worker and student class inherited
+        # the ordering from the meta subclasses of parent class
+        workers_age_list = Worker.objects.values("age")
+        self.assertSequenceEqual(
+            workers_age_list,
+            [
+                {"age": 40},
+                {"age": 45},
+            ],
+        )
+
+    def test_abstract_model_creation(self):
+        # abstract base classes can't create model instances of their own
+        with self.assertRaisesMessage(
+            AttributeError, "'CommonInfo' has no attribute 'objects'"
+        ):
+            CommonInfo.objects.all()
+
+    def test_model_with_distinct_related_query_name(self):
+        self.assertQuerysetEqual(
+            Post.objects.filter(testapp_comments__is_spam=True), []
+        )
+
+        # The Post model doesn't have a related query accessor based on
+        # related_name (testapp_comment_set).
+        msg = "Cannot resolve keyword 'testapp_comment_set' into field."
+        with self.assertRaisesMessage(FieldError, msg):
+            Post.objects.filter(testapp_comment_set__is_spam=True)
+
+    def test_multi_table_inheritance_equality(self):
+        # Equality doesn't transfer in multi-table inheritance.
+        self.assertNotEqual(Place(id=1), Restaurant(id=1))
+        self.assertNotEqual(Restaurant(id=1), Place(id=1))
+
+    def test_multi_table_inheritance_parent_child_one_to_one_link(self):
+        # multi-table inheritance relationship introduces links between the child model and
+        # each of its parents via an automatically-created OneToOneField
+        self.assertEqual(
+            Place.objects.get(name="Central Perk").restaurant,
+            Restaurant.objects.get(name="Central Perk"),
+        )
+        self.assertEqual(
+            Place.objects.get(name="McLarens").restaurant,
+            Restaurant.objects.get(name="McLarens"),
+        )
+    
+    def test_proxy_models(self):
+        test_person = Person.objects.create(first_name = "John", last_name="Doe")
+        test_my_person = MyPerson.objects.create(first_name = "Jane", last_name="Doe")
+        # The MyPerson class operates on the same database table as its parent Person class.
+        # In particular, any new instances of Person will also be accessible through MyPerson, and vice-versa
+        self.assertIsInstance(Person.objects.get(first_name='Jane'), Person)
+        self.assertIsInstance(MyPerson.objects.get(first_name='John'), MyPerson)
+
+    def test_ordered_proxy_models(self):
+        test_person = Person.objects.create(first_name = "John", last_name="Doe")
+        test_ordered_person = OrderedPerson.objects.create(first_name = "Jane", last_name="Dean")
+        last_name_list = OrderedPerson.objects.values('last_name')  
+        self.assertSequenceEqual(
+            last_name_list,
+            [
+                {'last_name':'Dean'},
+                {'last_name':'Doe'}
+            ]
+        )
+    
+    def test_multiple_inheritence(self):
+        self.assertEqual(
+            str(ProductReview.mro()),
+            "[<class 'testApp.models.ProductReview'>, <class 'testApp.models.Product'>, <class 'testApp.models.Review'>, <class 'django.db.models.base.Model'>, <class 'object'>]"
+
+        )
